3a1e93c4b2e88549884f464ba96a09b33462e8b5
yUm1k4 <zaiabdullah99@gmail.com>
2021-05-17 22:12:37 +0700

    final tutorial from yt

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,12 +104,18 @@ let areaCv = document.getElementById('area-cv')
 let resumeButton = document.getElementById('resume-button')
 
 // Html2pdf options
-
+let opt = {
+    margin:       0,
+    filename:     'Zainudin Abdullah - CV Resume.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 4 },
+    jsPDF:        { format: 'a4', orientation: 'portrait' }
+};
 
 // Function to call areaCv and Html2Pdf options 
-// function generateResume() {
-//     html2pdf(areaCv)
-// }
+function generateResume() {
+    html2pdf(areaCv, opt)
+}
 
 // When the button is clicked, it executes the three functions
 resumeButton.addEventListener('click', () => {
@@ -120,4 +126,5 @@ resumeButton.addEventListener('click', () => {
     generateResume()
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScale, 50000)
 })
